2451069ce3a2f566a2cf51b714b4d58ada9af0d3
chiaming yen <chiamingyen@gmail.com>
2014-12-15 19:59:53 +0800

    update setup Cherrypy, website version1

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+#@+leo-ver=5-thin
+#@+node:2015.20141215184040.1794: * @file setup.py
+#@@language python
+#@@tabwidth -4
+#@+others
+#@+node:2015.20141215184040.1796: ** setup declarations
+#@verbatim
+#@+leo-ver=5-thin
+#@verbatim
+#@+node:2015.20141215184040.1781: * @file setup.py
+#@verbatim
+#@@language python
+#@verbatim
+#@@tabwidth -4
+#@verbatim
+#@+others
+#@verbatim
+#@+node:2015.20141215184040.1776: ** setup declarations
 from setuptools import setup
 
 setup(name='YourAppName',
@@ -6,5 +24,11 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Cherrypy'],
      )
+#@-others
+#@verbatim
+#@-others
+#@verbatim
+#@-leo
+#@-leo
